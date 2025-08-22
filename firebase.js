9227a0092a2f5d3bedd244ad694d9b7eea90abfd
lucasrecebedoria <dragonreturn19@gmail.com>
2025-08-22 10:57:15 -0300

    Add files via upload

--- firebase.js
+++ firebase.js
@@ -1,1 +1,15 @@
-// Configuração Firebase
+// Firebase config
+const firebaseConfig = {
+  apiKey: "AIzaSyAxQliXm59BRg9zsVpK5qthB0nowqU0GEg",
+  authDomain: "lancamentomanual-6cac4.firebaseapp.com",
+  projectId: "lancamentomanual-6cac4",
+  storageBucket: "lancamentomanual-6cac4.firebasestorage.app",
+  messagingSenderId: "710102934933",
+  appId: "1:710102934933:web:a5dc954d01d40518a5c29c",
+  measurementId: "G-MLLXPXR7EC"
+};
+
+// Inicializa Firebase
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
+const db = firebase.firestore();
